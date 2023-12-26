72012cf673e42ec5cb2deb8e86446d6fa83e823a
seamice <unisa.dady@gmail.com>
2023-12-26 09:42:49 +0000

    Update from https://github.com/jet-w/jet-w.blog/commit/6dc627dd35535bf0b8b523351da49066990e6de3

---
+++ assets/02.selector.html-ba63c3ac.js
@@ -0,0 +1,115 @@
+import{_ as o}from"./plugin-vue_export-helper-c27b6911.js";import{r,o as u,c as p,e as i,w as n,f as c,b as e,d as s}from"./app-8263ff01.js";const d={},m=c('<h2 id="categories" tabindex="-1"><a class="header-anchor" href="#categories" aria-hidden="true">#</a> Categories</h2><ol><li><em><strong>Simple selectors</strong></em> (select elements based on name, id, class)</li><li><em><strong>Combinator selectors</strong></em> (select elements based on a specific relationship between them)</li><li><em><strong>Pseudo-class selectors</strong></em> (select elements based on a certain state)</li><li><em><strong>Pseudo-elements selectors</strong></em> (select and style a part of an element)</li><li><em><strong>Attribute selectors</strong></em> (select elements based on an attribute or attribute value)</li></ol><h3 id="simple-selectors" tabindex="-1"><a class="header-anchor" href="#simple-selectors" aria-hidden="true">#</a> Simple selectors</h3>',3),v=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token selector"},"p"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"text-align"),e("span",{class:"token punctuation"},":"),s(" center"),e("span",{class:"token punctuation"},";"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" red"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),b=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token selector"},".center"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"text-align"),e("span",{class:"token punctuation"},":"),s(" center"),e("span",{class:"token punctuation"},";"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" red"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),k=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token selector"},"#para1"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"text-align"),e("span",{class:"token punctuation"},":"),s(" center"),e("span",{class:"token punctuation"},";"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" red"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),h=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token selector"},"*"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"text-align"),e("span",{class:"token punctuation"},":"),s(" center"),e("span",{class:"token punctuation"},";"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" blue"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),g=e("h3",{id:"combinators",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#combinators","aria-hidden":"true"},"#"),s(" Combinators")],-1),_=e("p",null,[s("A CSS selector can contain more than one simple selector. Between the simple selectors, we can include a combinator."),e("br"),s(" There are four different combinators in CSS:")],-1),f=e("ol",null,[e("li",null,"descendant selector (space)"),e("li",null,"child selector (>)"),e("li",null,"adjacent sibling selector (+)"),e("li",null,"general sibling selector (~)")],-1),y=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token selector"},"h1, span"),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" green"),e("span",{class:"token punctuation"},";"),s(`
+  `),e("span",{class:"token property"},"font-size"),e("span",{class:"token punctuation"},":"),s(" 20px"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),x=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token selector"},"div.red"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"font-size"),e("span",{class:"token punctuation"},":"),s(" 30px"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),A=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token comment"},"/* select all span elements with div as root */"),s(`
+`),e("span",{class:"token selector"},"div span"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"font-size"),e("span",{class:"token punctuation"},":"),s(" 15px"),e("span",{class:"token punctuation"},";"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" orange"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),w=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token comment"},"/* select all the p elements with div as parent */"),s(`
+`),e("span",{class:"token selector"},"div > p"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"font-size"),e("span",{class:"token punctuation"},":"),s(" 15px"),e("span",{class:"token punctuation"},";"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" orange"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),S=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token comment"},"/* Select the adjacent element beside span, cannot be selected if exists other element between them*/"),s(`
+`),e("span",{class:"token selector"},"p + span"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" red"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),C=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token comment"},"/* select all the span after p of the same level*/"),s(`
+`),e("span",{class:"token selector"},"p ~ span"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" red"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),T=e("h3",{id:"pseudo-class-selectors",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#pseudo-class-selectors","aria-hidden":"true"},"#"),s(" Pseudo-class selectors")],-1),N=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token comment"},`/* 
+  first-child
+  last-child
+  nth-child(): 
+    if the parameter is n, means select all element
+    2n/even means select the even position elements
+    2n+1/odd means select the odd position elements
+  Note: All the elements above is sorted based on the children elements
+
+  :first-of-type
+  :last-of-type
+  :nth-of-type()
+  Note: The usuage is the same with the previous method, the different is sorted base on the same type
+
+  :not(): negative pseudo-class selectors：
+*/`),s(`
+
+`),e("span",{class:"token comment"},"/* select the first element of li with parnet ul */"),s(`
+`),e("span",{class:"token selector"},"ul > li:first-child"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" red"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+
+`),e("span",{class:"token comment"},"/* select the third element of li with parnet ul */"),s(`
+`),e("span",{class:"token selector"},"ul > li:not(:nth-child(3))"),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" orange"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),z=e("div",{class:"language-css line-numbers-mode","data-ext":"css"},[e("pre",{class:"language-css"},[e("code",null,[e("span",{class:"token comment"},"/* Hyperlink related */"),s(`
+`),e("span",{class:"token comment"},"/* the links that do not be accessed */"),s(`
+`),e("span",{class:"token selector"},"a:link"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" red"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`),e("span",{class:"token comment"},`/*
+   the links that have been accessed
+   Note: only the color can be modified because of privacy
+ */`),s(`
+`),e("span",{class:"token selector"},"a:visited"),s(),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" orange"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+
+`),e("span",{class:"token comment"},"/* :hover means the mouse move in */"),s(`
+`),e("span",{class:"token selector"},"a:hover"),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" aqua"),e("span",{class:"token punctuation"},";"),s(`
+  `),e("span",{class:"token property"},"font-size"),e("span",{class:"token punctuation"},":"),s(" 50px"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+
+`),e("span",{class:"token comment"},"/* :active means has been clicked*/"),s(`
+`),e("span",{class:"token selector"},"a:hover"),e("span",{class:"token punctuation"},"{"),s(`
+  `),e("span",{class:"token property"},"color"),e("span",{class:"token punctuation"},":"),s(" yellowgreen"),e("span",{class:"token punctuation"},";"),s(`
+`),e("span",{class:"token punctuation"},"}"),s(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),U=c(`<h3 id="pseudo-elements-selectors" tabindex="-1"><a class="header-anchor" href="#pseudo-elements-selectors" aria-hidden="true">#</a> Pseudo-elements selectors</h3><h3 id="attribute-selectors" tabindex="-1"><a class="header-anchor" href="#attribute-selectors" aria-hidden="true">#</a> Attribute selectors</h3><div class="language-css line-numbers-mode" data-ext="css"><pre class="language-css"><code><span class="token comment">/* Attribute selector: Select all p elements with attribute title */</span>
+<span class="token selector">p[title]</span><span class="token punctuation">{</span>
+  <span class="token property">color</span><span class="token punctuation">:</span> orange<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment">/* Attribute selector: Select all p elements with attribute title=abc */</span>
+<span class="token selector">p[title=abc]</span><span class="token punctuation">{</span>
+  <span class="token property">color</span><span class="token punctuation">:</span> orange<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment">/* Attribute selector: Select all p elements with attribute title with value start with abc */</span>
+<span class="token selector">p[title^=abc]</span><span class="token punctuation">{</span>
+  <span class="token property">color</span><span class="token punctuation">:</span> orange<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment">/* Attribute selector: Select all p elements with attribute title with value end with abc */</span>
+<span class="token selector">p[title$=abc]</span><span class="token punctuation">{</span>
+  <span class="token property">color</span><span class="token punctuation">:</span> orange<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment">/* Attribute selector: Select all p elements with attribute title with value contains abc */</span>
+<span class="token selector">p[title*=abc]</span><span class="token punctuation">{</span>
+  <span class="token property">color</span><span class="token punctuation">:</span> orange<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="the-priority-of-selectors" tabindex="-1"><a class="header-anchor" href="#the-priority-of-selectors" aria-hidden="true">#</a> The priority of selectors</h1><p>Inherit styles &lt; Universal selectors &lt; element selectors &lt; class and pseudo clss selector &lt; id selector &lt; inline selector</p><p>When comparing priorities, the priorities of all selectors need to be added together (calculated separately when grouping selectors). The accumulation of selectors will not exceed its maximum magnitude (the accumulation of class selectors will not exceed the id selection). device), if the priorities are the same, the lower style will be used limitedly.</p><div class="hint-container info"><p class="hint-container-title">Info</p><p>The <code>!important</code> can be added to the end of a style, then the style will get the highest priority, even exceed inline style.</p></div>`,7);function j(B,I){const l=r("CodeTabs");return u(),p("div",null,[m,i(l,{id:"33",data:[{id:"element selector"},{id:"class selector"},{id:"id selector"},{id:"Universal selectors"}]},{title0:n(({value:a,isActive:t})=>[s("element selector")]),title1:n(({value:a,isActive:t})=>[s("class selector")]),title2:n(({value:a,isActive:t})=>[s("id selector")]),title3:n(({value:a,isActive:t})=>[s("Universal selectors")]),tab0:n(({value:a,isActive:t})=>[v]),tab1:n(({value:a,isActive:t})=>[b]),tab2:n(({value:a,isActive:t})=>[k]),tab3:n(({value:a,isActive:t})=>[h]),_:1}),g,_,f,i(l,{id:"75",data:[{id:"Union"},{id:"Intersect"},{id:"Descendant"},{id:"Child"},{id:"Adjacent sibling"},{id:"General sibling"}]},{title0:n(({value:a,isActive:t})=>[s("Union")]),title1:n(({value:a,isActive:t})=>[s("Intersect")]),title2:n(({value:a,isActive:t})=>[s("Descendant")]),title3:n(({value:a,isActive:t})=>[s("Child")]),title4:n(({value:a,isActive:t})=>[s("Adjacent sibling")]),title5:n(({value:a,isActive:t})=>[s("General sibling")]),tab0:n(({value:a,isActive:t})=>[y]),tab1:n(({value:a,isActive:t})=>[x]),tab2:n(({value:a,isActive:t})=>[A]),tab3:n(({value:a,isActive:t})=>[w]),tab4:n(({value:a,isActive:t})=>[S]),tab5:n(({value:a,isActive:t})=>[C]),_:1}),T,i(l,{id:"98",data:[{id:"Element"},{id:"Hyperlink"}]},{title0:n(({value:a,isActive:t})=>[s("Element")]),title1:n(({value:a,isActive:t})=>[s("Hyperlink")]),tab0:n(({value:a,isActive:t})=>[N]),tab1:n(({value:a,isActive:t})=>[z]),_:1}),U])}const E=o(d,[["render",j],["__file","02.selector.html.vue"]]);export{E as default};
--- assets/03.MotifsInTemporalNetworks.html-b1786cfe.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o,c as t}from"./app-68eddc36.js";const r={};function _(c,s){return o(),t("div")}const f=e(r,[["render",_],["__file","03.MotifsInTemporalNetworks.html.vue"]]);export{f as default};
