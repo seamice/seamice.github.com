d9df853dbc1d434bf2c6126a25b6b34119617291
seamice <unisa.dady@gmail.com>
2024-01-13 23:01:07 +0000

    Update from https://github.com/jet-w/jet-w.blog/commit/b57f3db410c476b715eb89a027edef0da793cb35

---
+++ src/js/gtag.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DEMNZ1E6YS');
+});
